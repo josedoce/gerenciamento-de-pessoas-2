e2eb87e640402dba6c81b3985c44a3023ffd9961
josedoce <josesamya@gmail.com>
2021-08-10 13:46:08 -0300

    testes do projeto

---
+++ src/test/java/com/gpessoas/doispontozero/DoispontozeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpessoas.doispontozero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoispontozeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
