37955bf3415d59d4c4aeebfe0b0254a6f59182f1
josedoce <josesamya@gmail.com>
2021-08-10 13:48:29 -0300

    classe inicializadora do projeto

---
+++ src/main/java/com/gpessoas/doispontozero/DoispontozeroApplication.java
@@ -0,0 +1,13 @@
+package com.gpessoas.doispontozero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoispontozeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoispontozeroApplication.class, args);
+	}
+
+}
